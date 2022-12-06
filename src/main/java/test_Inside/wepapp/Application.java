a22d8201a8aaac89c496095fbbe1b4e31e2bf603
Mariya Korotkevich <ma.korotkevich@gmail.com>
2022-12-06 11:59:11 +0300

    init commit

---
+++ src/main/java/test_Inside/wepapp/Application.java
@@ -0,0 +1,15 @@
+package test_Inside.wepapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("test_Inside")
+@EntityScan("test_Inside")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
